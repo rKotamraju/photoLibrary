42000590c6c616fa9a91ca366ae85b09d2fbf8c6
rKotamraju <50004789+rKotamraju@users.noreply.github.com>
2020-04-01 00:40:11 -0400

    fixed compiler errors

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("My Photos");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
