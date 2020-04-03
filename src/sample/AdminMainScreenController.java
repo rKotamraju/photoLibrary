66c703e997a34098e2b1bca49c946b625e17ccb5
rKotamraju <50004789+rKotamraju@users.noreply.github.com>
2020-04-03 00:27:07 -0400

    finished connecting first three screens

--- src/sample/AdminMainScreenController.java
+++ src/sample/AdminMainScreenController.java
@@ -1,10 +1,49 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminMainScreenController {
- @FXML
-    private Label test;
+    //Declaration of all buttons
+     @FXML
+        private Button logOutButton;
+
+     @FXML
+     private Button deleteUserButton;
+
+     @FXML
+     private Button createNewUserButton;
+
+     //Declaration of ListView
+    @FXML
+    private ListView usersListView;
+
+
+     @FXML
+        private void logOutPressed(ActionEvent e) throws IOException {
+
+         System.out.println("Logging out from Admin Main Screen");
+         Stage stage = null;
+         Parent root = null;
+
+         if(e.getSource() == logOutButton){
+             stage = (Stage) logOutButton.getScene().getWindow();
+             root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+         }
+
+         Scene scene = new Scene(root);
+         stage.setScene(scene);
+         stage.show();
+
+     }
 
 }
--- src/sample/AlbumsMainScreenController.java
+++ src/sample/AlbumsMainScreenController.java
@@ -1,4 +1,58 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AlbumsMainScreenController {
+
+    //Declaration of all buttons
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private Button editAlbumButton;
+
+    @FXML
+    private Button renameAlbumButton;
+
+    @FXML
+    private Button deleteAlbumButton;
+
+    @FXML
+    private Button createAlbumButton;
+
+    //Declaration of TextField
+    @FXML
+    private TextField searchAlbumsTextField;
+
+    //Declaration of ListView
+    @FXML
+    private ListView albumsListView;
+
+    @FXML
+    private void logOutPressed(ActionEvent e) throws IOException {
+        System.out.println("Logging out from albums main screen");
+
+        Stage stage = null;
+        Parent root = null;
+
+        if(e.getSource() == logOutButton){
+            stage = (Stage) logOutButton.getScene().getWindow();
+
+            root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        }
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
