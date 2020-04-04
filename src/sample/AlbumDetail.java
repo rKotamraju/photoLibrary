afbf21548844fe51199b2e10faf1e9dad8f0a806
Sujit <sujit.mollet@rutgers.edu>
2020-04-03 23:32:17 -0400

    working on displaying AlbumDetailScreen

---
+++ src/sample/AlbumDetail.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class AlbumDetail {
+    String name;
+    int count;
+
+    public AlbumDetail(String name){
+        this.name = name;
+    }
+
+    public String toString(){
+        return name;
+    }
+}
--- src/sample/AlbumsMainScreenController.java
+++ src/sample/AlbumsMainScreenController.java
@@ -1,43 +1,57 @@
 package sample;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.fxml.Initializable;
+
 
 import java.io.IOException;
+import java.util.ResourceBundle;
+import java.net.URL;
 
-public class AlbumsMainScreenController {
+import static javafx.collections.FXCollections.observableArrayList;
 
-    //Declaration of all buttons
+public class AlbumsMainScreenController implements Initializable{
+
+//BUTTONS
     @FXML
     private Button logOutButton;
-
     @FXML
     private Button editAlbumButton;
-
     @FXML
     private Button renameAlbumButton;
-
     @FXML
     private Button deleteAlbumButton;
-
     @FXML
     private Button createAlbumButton;
 
-    //Declaration of TextField
+//TEXTFIELDS
     @FXML
     private TextField searchAlbumsTextField;
 
-    //Declaration of ListView
+//LISTVIEW
     @FXML
-    private ListView albumsListView;
+    private ListView AlbumsListView;
 
+    final ObservableList<AlbumDetail> albumsObservableList = observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        editAlbumButton.setDisable(true);
+        renameAlbumButton.setDisable(true);
+        deleteAlbumButton.setDisable(true);
+
+        System.out.println("Intitializing");
+        AlbumsListView.setItems(albumsObservableList);
+    }
+
+//ONCLICK METHODS
     @FXML
     private void logOutPressed(ActionEvent e) throws IOException {
         System.out.println("Logging out from albums main screen");
@@ -55,4 +69,28 @@ public class AlbumsMainScreenController {
         stage.setScene(scene);
         stage.show();
     }
+
+
+    @FXML
+    private void createAlbumPressed(ActionEvent e){
+
+        TextInputDialog td = new TextInputDialog();
+        td.setHeaderText("Enter an Album Name.");
+        td.showAndWait();
+
+        if(td.getResult() == null){
+            return;
+        }
+
+        String albumName = td.getResult().trim();
+        if(albumName.length() == 0){
+            return;
+        }
+
+        AlbumDetail newAlbum = new AlbumDetail(albumName);
+        albumsObservableList.add(newAlbum);
+
+
+    }
+
 }
