07d50dfec4748230b81694db0568a05fefbc5c03
rKotamraju <50004789+rKotamraju@users.noreply.github.com>
2020-04-13 00:25:08 -0400

    hi

--- src/sample/AdminController.java
+++ src/sample/AdminController.java
@@ -5,7 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;admi
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
